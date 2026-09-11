8c33c0920e6e31895170eaf0be39c7d61029cb55
sutharshikhar-gif <sutharshikhar@gmail.com>
2026-09-11 22:03:13 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cout << "Enter a positive integer: ";
+   cin >> n;
+
+   for (int i =1; i <= n; i++) {
+      cout << i<<endl;
+   }
+   
+
+
+
+ return 0;
+}

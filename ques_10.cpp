583b58ffca0ea66ea2c39d9e7458ec6f2977d199
sutharshikhar-gif <sutharshikhar@gmail.com>
2026-09-12 21:51:22 +0530

    Add files via upload

---
+++ ques_10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a year: ";
+    cin >> n;
+
+    if ( (n % 400 == 0) || (n % 4 == 0 && n % 100 != 0) ) {
+        cout << n << " is a leap year." << endl;
+    } else {
+        cout << n << " is not a leap year." << endl;
+    }
+
+    return 0;
+}
